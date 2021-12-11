88bceb008a8f5507af4ae84edde0d5913ec320ca
Huy Đào <huydqth2009022@fpt.edu.vn>
2021-12-11 23:46:55 +0700

    Assignment WAD

---
+++ Assignment-WAD-master/Assignment WAD/Controllers/StudentsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_WAD.Data;
+using Assignment_WAD.Models;
+
+namespace Assignment_WAD.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private Assignment_WADContext db = new Assignment_WADContext();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,Formality,Money,PublictDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentID,Formality,Money,PublictDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [HttpGet]
+        public ActionResult Register(Student student)
+        {
+            // It should be better to read this data from database
+            ViewBag.Fomality = new SelectList(
+                                           new List<SelectListItem>
+                                           {
+                                        new SelectListItem { Text = "1368", Value = "Chống Đẩy" },
+                                        new SelectListItem { Text = "1369", Value = "Nộp Tiền"}, //....
+                                           }, "Value", "Text");
+            // Also other properties like Month and Day
+            return View();
+        }
+    }
+}
---
+++ Assignment-WAD-master/Assignment WAD/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_WAD.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string Formality { get; set; }
+        public string Money { get; set; }
+        public DateTime PublictDate { get; set; }
+    }
+}
